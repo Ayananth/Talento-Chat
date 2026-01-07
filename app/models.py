f2667460210b0520932bbc43cb08f30d0f665b7e
Ayananth <tsayananth93@gmail.com>
2026-01-07 12:03:28 +0530

    database connections added, models created, tables created

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from database import Base
+
+class Conversation(Base):
+    __tablename__ = "conversations"
+
+    id = Column(Integer, primary_key=True)
+    job_id = Column(Integer, nullable=False)
+    jobseeker_id = Column(Integer, nullable=False)
+    recruiter_id = Column(Integer, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True)
+    conversation_id = Column(
+        Integer,
+        ForeignKey("conversations.id"),
+        nullable=False
+    )
+    sender_id = Column(Integer, nullable=False)
+    content = Column(Text, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
