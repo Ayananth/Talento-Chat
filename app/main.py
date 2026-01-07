8a977af9502eac1c8a9b79cb4a52548ff12fca6b
Ayananth <tsayananth93@gmail.com>
2026-01-07 09:45:14 +0530

    sample web socket end point created and tested

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 
 app = FastAPI()
 
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "FastAPI is running"}
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+
+    while True:
+        data = await websocket.receive_text()
+        print("Received:", data)
+        await websocket.send_text(f"Echo: {data} from server")
